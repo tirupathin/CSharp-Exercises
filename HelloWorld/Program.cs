83abcb8f8e6274fa390bd28d3b0f38c29b586c9d
tirupathin <tnaraharisetty@gmal.com>
2024-11-18 13:37:24 -0500

    Exercises 1-6

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Learning C# - Om");
+
+//Exercise The Makings of a Programmer
+
+// Console.WriteLine("This is the First statement");
+// Console.WriteLine("This is the second statement");
+// Console.WriteLine("This is the third statement");
+// Console.WriteLine("This is the fourth statement");
+// Console.WriteLine("This is the fifth and final statement");
+// Console.WriteLine ("This is for the extension check");
+
+//Exercise Consolas and Telim
+
+// string name;
+// Console.WriteLine ("Bread is ready");
+// Console.WriteLine ("Who is the bread for?");
+// name = Console.ReadLine ();
+// Console.WriteLine ($"Noted : {name} got bread");
+
+
+//Exercise The Thing Namer 3000
+
+// Console.WriteLine("What kind of thing are we talking about?");
+// string? a = Console.ReadLine();
+// Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// string? b = Console.ReadLine();
+// string c = " of Doom";
+// string d = "3000";
+// Console.WriteLine("The " + b + " " + a  + c + " " + d + "!");
+
+// Exercise The Variable Shop
+
+bool flag = true;
+byte b= 67;
+short s = -32455;
+int i = -2345;
+long l = -939393933933;
+sbyte sb=-121;
+ushort us=32222;
+uint ui =23455;
+ulong ul = 9202022200L;
+float f = 3.22f;
+double d = 98.23455677777666689444444777;
+decimal de = -9.345m;
+char ch = 'a';
+string name = "tiru";
+
+Console.WriteLine("bool value is " + flag);
+Console.WriteLine("byte value is " + b);
+Console.WriteLine("short value is " + s);
+Console.WriteLine("int value is " + i);
+Console.WriteLine("long value is " + l);
+Console.WriteLine("signed byte value is " + sb);
+Console.WriteLine("unsigned short value is " + us);
+Console.WriteLine("unsigned long value is " + ul);
+Console.WriteLine("unsigned int value is " + ui);
+Console.WriteLine("float value is " + f);
+Console.WriteLine("double value is " + d);
+Console.WriteLine("decimal value is " + de);
+Console.WriteLine("char value is " + ch);
+Console.WriteLine("string value is " + name);
+
+
+// Exercise The Variable Shop Returns
+
+ flag = false;
+ b= 17;
+ s = -30333;
+ i = -23452;
+ l = -9393933933;
+ sb=124;
+ us=32112;
+ ui =23445;
+ ul = 9203022200L;
+ f = 30.212f;
+ d = 98.23457777666689444444777;
+ de = -9.345557888m;
+ ch = 'b';
+ name = "vbp";
+
+Console.WriteLine("bool value is " + flag);
+Console.WriteLine("byte value is " + b);
+Console.WriteLine("short value is " + s);
+Console.WriteLine("int value is " + i);
+Console.WriteLine("long value is " + l);
+Console.WriteLine("signed byte value is " + sb);
+Console.WriteLine("unsigned short value is " + us);
+Console.WriteLine("unsigned long value is " + ul);
+Console.WriteLine("unsigned int value is " + ui);
+Console.WriteLine("float value is " + f);
+Console.WriteLine("double value is " + d);
+Console.WriteLine("decimal value is " + de);
+Console.WriteLine("char value is " + ch);
+Console.WriteLine("string value is " + name);
---
+++ MathExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Exercise The Triangle Farmer");
+
+// float b = 0.00f;
+// float height=0.00f;
+// float area=0.00f;
+
+// Console.WriteLine("Enter the Length of Base:");
+// b = Convert.ToSingle(Console.ReadLine());
+// Console.WriteLine("Enter the Length of Height:");
+// height = Convert.ToSingle(Console.ReadLine());
+
+// area = (b*height) / 2;
+// Console.WriteLine($"Area of the triangle is {area} ");
+
+Console.WriteLine("Exercise The Four Sisters and the Duckbear");
+int totalChocolateEggs =0;
+int eggsForEachSister =0;
+int eggsForDuckbear= 0;
+
+Console.WriteLine("Enter Total Number of Eggs: ");
+totalChocolateEggs=Convert.ToInt32(Console.ReadLine());
+
+eggsForDuckbear = totalChocolateEggs%4;
+eggsForEachSister = totalChocolateEggs/4;
+
+Console.WriteLine($"Eggs for Each Sister :  {eggsForEachSister}");
+Console.WriteLine($"Eggs for Duck Bear :  {eggsForDuckbear}");
